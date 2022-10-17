278d61fa633dd72e6e0c2d35d227322d907844d3
EladReuven <102731503+EladReuven@users.noreply.github.com>
2022-10-18 01:26:49 +0300

    camera moves to sides when hovering over sides

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -8,22 +8,82 @@ public class CameraMovement : MonoBehaviour
     public Quaternion roadViewRot;
     public Quaternion mapViewRot;
 
+    private Quaternion carRot;
+    private Quaternion camStartingRot;
+    private Quaternion offset;
+    [SerializeField] private CameraView currentView;
+
+    private enum CameraView
+    {
+        left,
+        middle,
+        right
+    }
+
+    private void Start()
+    {
+        currentView = CameraView.middle;
+        camStartingRot = Camera.main.transform.rotation;
+    }
+
+    private void Update()
+    {
+        offset = new Quaternion(camStartingRot.x - Camera.main.transform.rotation.x,
+            camStartingRot.y - Camera.main.transform.rotation.y,
+            camStartingRot.z - Camera.main.transform.rotation.z,
+            camStartingRot.w - Camera.main.transform.rotation.w);
+
+        TurnCamera();
+    }
+
+    
+
+    private void TurnCamera()
+    {
+        if (Input.mousePosition.x < 10 && currentView != CameraView.left)
+        {
+            OrientMapView();
+        }
+        else if (Input.mousePosition.x > Screen.width - 10 && currentView != CameraView.right)
+        {
+            OrientObjView();
+        }
+        else if (Input.mousePosition.x >= 10 && Input.mousePosition.x <= Screen.width - 10 && currentView != CameraView.middle)
+        {
+            OrientRoadView();
+        }
+    }
+
     [ContextMenu("orient to object View")]
     public void OrientObjView()
     {
-        Camera.main.transform.rotation = objectViewRot;
+        currentView = CameraView.right;
+        Camera.main.transform.localRotation = objectViewRot;
     }
 
     [ContextMenu("orient to road View")]
     public void OrientRoadView()
     {
-        Camera.main.transform.rotation = roadViewRot;
+        currentView = CameraView.middle;
+        Camera.main.transform.localRotation = roadViewRot;
     }
 
     [ContextMenu("orient to map View")]
     public void OrientMapView()
     {
-        Camera.main.transform.rotation = mapViewRot;
+        currentView = CameraView.left;
+        Camera.main.transform.localRotation = mapViewRot;
     }
 
+    [ContextMenu("quaternion check")]
+    public void QuaternionCheck()
+    {
+        Debug.Log("normal cam rot: " + Camera.main.transform.rotation);
+        Debug.Log("starting cam rot: " + camStartingRot);
+        Debug.Log("LOCAL cam rot: " + Camera.main.transform.localRotation);
+        Debug.Log("obj view: " + objectViewRot);
+        Debug.Log("road view: " + roadViewRot);
+        Debug.Log("map view: " + mapViewRot);
+        Debug.Log("offset: " + offset);
+    }
 }
